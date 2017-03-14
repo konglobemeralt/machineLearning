dff75767110933552265c4ade13ae5160947e715
konglobemeralt <jesper.blidkvist@live.se>
2017-03-14 17:48:02 +0100

    Implemented Feature normalization

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -27,7 +27,18 @@ sigma = zeros(1, size(X, 2));
 %       
 
 
+m = size(X, 1);
+mu = mean(X);
 
+for i = 1 : m
+    X_norm(i, :) = X(i, :) - mu;
+end
+
+sigma = std(X);
+
+for i = 1 : m
+    X_norm(i, :) = X_norm(i, :) ./ sigma;
+end
 
 
 
