02420658382ef63260a38d0d944a0f4daeaded70
konglobemeralt <jesper.blidkvist@live.se>
2017-03-28 18:58:30 +0200

    added all the finished excercises for w4

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,9 +29,9 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
+predict_all = X * all_theta';
 
-
-
+[p_max,p] = max(predict_all, [] , 2);
 
 
 
